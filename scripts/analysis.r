f94f0f1512012fb0d5fc3fcb6e2e10bb4d3e2872
Abhishek Joshi <joshi.abhishek@hotmail.com>
2016-03-01 20:47:03 -0800

    Answered first question

--- scripts/analysis.r
+++ scripts/analysis.r
@@ -3,7 +3,12 @@ library("dplyr")
 
 analysis <- function(dataset) {
 
-  data <- read.csv("../data/Top_Ten_US_Causes_Of_Death.csv")
+  #data <- read.csv("../data/Top_Ten_US_Causes_Of_Death.csv")
+  
+  data <- read.csv("C:/Users/Abhishek/Documents/GitHub/final-project-INFO498F/data/Top_Ten_US_Causes_Of_Death.csv")
+  
+  data[,5] <- as.numeric(as.character(data[,5]))
+  
   
   #Gets Causes of Death
   unique_causes_of_death <- data %>% distinct(CAUSE_NAME) %>% select(CAUSE_NAME)
@@ -13,8 +18,14 @@ analysis <- function(dataset) {
   
   #What have been the leading causes of death in order from 1999 to 2013?
   
+  death_causes_grouped_by_year <- data %>% group_by(YEAR) 
+  print(death_causes_grouped_by_year)
   
+  death_causes_by_name_by_year <- death_causes_grouped_by_year %>% group_by(CAUSE_NAME)
+  print(year_grouped_death_causes)
   
+  total_sum_by_death_cause <- death_causes_by_name_by_year %>% filter(STATE == 'United States') %>% select(DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
+  print(total_sum_by_death_cause)
   
   #What causes of death have grown the most in the past 14 years from 1999 to 2013?
   
