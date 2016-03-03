3bbaea8fa0902a092563d79dd1e1a00236ae895d
Jonathan Ieng <Jonathan Ieng>
2016-03-03 14:14:41 -0800

    Added about me tab

--- ui.r
+++ ui.r
@@ -106,4 +106,89 @@ shinyUI(
                         
                       ))
     )
+<<<<<<< 1528845cc2143de07f009285a8c85a44a62c057e
   )
+=======
+  ),
+  tabPanel("Map",
+     sidebarLayout(
+       
+       sidebarPanel(
+         # Widget 1: slider input for year
+         sliderInput("year", label = h3("Year"), min = 1999, 
+          max = 2013, value = 1, sep = "", animate= animationOptions(interval=500, loop=TRUE)
+         ),
+           
+         
+         
+         # Widget 2: Select box for cause
+         selectInput("causes", label = h3("Choose a cause of death:"), 
+                     choices = list("All Causes" = 'All Causes', "Alzheimer's disease" = "Alzheimer's disease", "Cancer" = 'Cancer', 
+                                    "Chronic liver disease and cirrhosis" = 'Chronic liver disease and cirrhosis', "Chronic Lower Respiratory Disease (CLRD)" = 'CLRD',
+                                    "Diabetes" = 'Diabetes', "Diseases of Heart" = 'Diseases of Heart', "Essential hypertension and hypertensive renal disease" = 'Essential hypertension and hypertensive renal disease',
+                                    "Homicide" = 'Homicide', "Influenza and pneumonia" = 'Influenza and pneumonia', "Kidney Disease" = 'Kidney Disease', "Parkinson's disease" = "Parkinson's disease", 
+                                    "Pneumonitis due to solids and liquids" = 'Pneumonitis due to solids and liquids', "Septicemia" = 'Septicemia', "Stroke" = 'Stroke', "Suicide" = 'Suicide', "Unintentional Injuries" = 'Unintentional Injuries') 
+         )
+       ),
+       
+       mainPanel(
+         plotlyOutput("map")
+       ) 
+       
+       
+     )
+  
+  ),
+  
+  tabPanel("Analysis",
+     sidebarLayout(
+       
+       sidebarPanel(
+         selectInput("year2", label = h3("Choose a year:"), 
+                     choices = list("1999" = '1999', "2000" = '2000', "2001" = '2001', "2002" = '2002',
+                                    "2003" = '2003', "2004" = '2004', "2005" = '2005', "2006" = '2006',
+                                    "2007" = '2007', "2008" = '2008', "2009" = '2009', "2010" = '2010',
+                                    "2011" = '2011', "2012" = '2012', "2013" = '2013'), 
+                     selected = "1999")
+       ),
+       
+       mainPanel(
+         plotlyOutput("pie")
+         #verbatimTextOutput("analysis")
+       ) 
+       
+       
+     )
+           
+  ),
+  tabPanel("Bar Graph",
+     sidebarLayout(
+       
+       sidebarPanel(
+       ),
+       
+       mainPanel(
+         plotlyOutput("bar_graph")
+       ) 
+       
+       
+     )
+           
+  ),
+  
+  tabPanel("About me",
+           sidebarLayout(
+             
+             sidebarPanel(
+             ),
+             
+             mainPanel(
+               
+             ) 
+             
+             
+           )
+           
+  )
+))
+>>>>>>> Added about me tab
