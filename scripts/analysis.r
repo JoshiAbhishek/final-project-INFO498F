d50177c5c87c62049d3291c1c1c80b7d4f12632b
Abhishek Joshi <joshi.abhishek@hotmail.com>
2016-03-03 12:26:08 -0800

    Formatted code

--- scripts/analysis.r
+++ scripts/analysis.r
@@ -1,6 +1,6 @@
 library("dplyr")
 
-#Questions: 
+#Questions:
 
 #What have been the leading causes of death in order from 1999 to 2013?
 
@@ -10,89 +10,122 @@ library("dplyr")
 
 #What is the proportional relation between causes of death most recently? How has it changed in the past 14 years from 1999 to 2013
 
+#Returns the unique causes of death names
 
 Unique_Death_Causes <- function(data) {
-  unique_causes_of_death <- data %>% distinct(CAUSE_NAME) %>% select(CAUSE_NAME)
+  unique_causes_of_death <-
+    data %>% distinct(CAUSE_NAME) %>% select(CAUSE_NAME)
   
   return(unique_causes_of_death)
 }
 
+#Returns the total number of deaths caused from 1999 to 2013, by the cause of death
 Death_Cause_Totals <- function(data) {
   data[,5] <- as.numeric(as.character(data[,5]))
   
-  death_causes_grouped_by_year <- data %>% group_by(YEAR) 
+  death_causes_grouped_by_year <- data %>% group_by(YEAR)
   
-  death_causes_by_name_by_year <- death_causes_grouped_by_year %>% group_by(CAUSE_NAME)
+  death_causes_by_name_by_year <-
+    death_causes_grouped_by_year %>% group_by(CAUSE_NAME)
   
-  total_sum_by_death_cause <- death_causes_by_name_by_year %>% filter(STATE == 'United States') %>% select(DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
+  total_sum_by_death_cause <-
+    death_causes_by_name_by_year %>% filter(STATE == 'United States') %>%
+    select(DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
   
   return (total_sum_by_death_cause)
 }
 
+#Returns the difference in death totals from 1999 to 2013 by cause of death
 Diff_2013_1999 <- function() {
   data[,5] <- as.numeric(as.character(data[,5]))
   
-  deaths_filtered <- data %>% filter(STATE == 'United States') %>% select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
+  deaths_filtered <-
+    data %>% filter(STATE == 'United States') %>%
+    select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
   
-  deaths_1999 <- deaths_filtered %>% filter(YEAR == '1999') %>% select(YEAR, CAUSE_NAME, DEATHS)
+  deaths_1999 <-
+    deaths_filtered %>% filter(YEAR == '1999') %>% select(YEAR, CAUSE_NAME, DEATHS)
   
-  deaths_2013 <- deaths_filtered %>% filter(YEAR == '2013') %>% select(YEAR, CAUSE_NAME, DEATHS)
+  deaths_2013 <-
+    deaths_filtered %>% filter(YEAR == '2013') %>% select(YEAR, CAUSE_NAME, DEATHS)
   
-  deaths_2013 <- mutate(deaths_2013, Diff = deaths_2013$DEATHS - deaths_1999$DEATHS)
+  deaths_2013 <-
+    mutate(deaths_2013, Diff = deaths_2013$DEATHS - deaths_1999$DEATHS)
   
-  deaths_diff_2013_1999 <- deaths_2013 <- deaths_2013 %>% select(CAUSE_NAME, Diff)
+  deaths_diff_2013_1999 <-
+    deaths_2013 <- deaths_2013 %>% select(CAUSE_NAME, Diff)
   
   return(deaths_diff_2013_1999)
 }
 
+#Returns the cause of death with the maximum growth from 1999 to 2013
 Max_Cause_Growth <- function(data) {
-  
   data[,5] <- as.numeric(as.character(data[,5]))
   
-  deaths_filtered <- data %>% filter(STATE == 'United States') %>% select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
+  deaths_filtered <-
+    data %>% filter(STATE == 'United States') %>%
+    select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
   
-  deaths_1999 <- deaths_filtered %>% filter(YEAR == '1999') %>% select(YEAR, CAUSE_NAME, DEATHS)
+  deaths_1999 <-
+    deaths_filtered %>% filter(YEAR == '1999') %>% select(YEAR, CAUSE_NAME, DEATHS)
   
-  deaths_2013 <- deaths_filtered %>% filter(YEAR == '2013') %>% select(YEAR, CAUSE_NAME, DEATHS)
+  deaths_2013 <-
+    deaths_filtered %>% filter(YEAR == '2013') %>% select(YEAR, CAUSE_NAME, DEATHS)
   
-  deaths_2013 <- mutate(deaths_2013, Diff = deaths_2013$DEATHS - deaths_1999$DEATHS)
+  deaths_2013 <-
+    mutate(deaths_2013, Diff = deaths_2013$DEATHS - deaths_1999$DEATHS)
   
-  deaths_diff_2013_1999 <- deaths_2013 <- deaths_2013 %>% select(CAUSE_NAME, Diff)
+  deaths_diff_2013_1999 <-
+    deaths_2013 <- deaths_2013 %>% select(CAUSE_NAME, Diff)
   
-  max_death_cause_growth <- deaths_diff_2013_1999 %>% filter(Diff == max(Diff))
+  max_death_cause_growth <-
+    deaths_diff_2013_1999 %>% filter(Diff == max(Diff))
   
   return(max_death_cause_growth)
 }
 
+#Returns the cause of death with the minimum growth from 1999 to 2013
 Min_Cause_Growth <- function() {
-  
   data[,5] <- as.numeric(as.character(data[,5]))
   
-  deaths_filtered <- data %>% filter(STATE == 'United States') %>% select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
+  deaths_filtered <-
+    data %>% filter(STATE == 'United States') %>%
+    select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
   
-  deaths_1999 <- deaths_filtered %>% filter(YEAR == '1999') %>% select(YEAR, CAUSE_NAME, DEATHS)
+  deaths_1999 <-
+    deaths_filtered %>% filter(YEAR == '1999') %>% select(YEAR, CAUSE_NAME, DEATHS)
   
-  deaths_2013 <- deaths_filtered %>% filter(YEAR == '2013') %>% select(YEAR, CAUSE_NAME, DEATHS)
+  deaths_2013 <-
+    deaths_filtered %>% filter(YEAR == '2013') %>% select(YEAR, CAUSE_NAME, DEATHS)
   
-  deaths_2013 <- mutate(deaths_2013, Diff = deaths_2013$DEATHS - deaths_1999$DEATHS)
+  deaths_2013 <-
+    mutate(deaths_2013, Diff = deaths_2013$DEATHS - deaths_1999$DEATHS)
   
-  deaths_diff_2013_1999 <- deaths_2013 <- deaths_2013 %>% select(CAUSE_NAME, Diff)
+  deaths_diff_2013_1999 <-
+    deaths_2013 <- deaths_2013 %>% select(CAUSE_NAME, Diff)
   
-  min_death_cause_growth <- deaths_diff_2013_1999 %>% filter(Diff == min(Diff))
+  min_death_cause_growth <-
+    deaths_diff_2013_1999 %>% filter(Diff == min(Diff))
   
   return(min_death_cause_growth)
 }
 
+#Returns the data, filtered by the country being the United States and the cause of death being everything but "All Causes"
 Deaths_Filtered <- function(data) {
   data[,5] <- as.numeric(as.character(data[,5]))
   
-  deaths_filtered <- data %>% filter(STATE == 'United States') %>% select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
+  deaths_filtered <-
+    data %>% filter(STATE == 'United States') %>%
+    select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
   
   return(deaths_filtered)
 }
 
+#Returns a pie chart showing the proportional deaths by cause of death for the given year
 Build_Yearly_Deaths_Chart <- function(data, year) {
-  deaths_filtered <- data %>% filter(STATE == 'United States') %>% select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
+  deaths_filtered <-
+    data %>% filter(STATE == 'United States') %>%
+    select(YEAR, CAUSE_NAME, DEATHS) %>% filter(CAUSE_NAME != 'All Causes')
   
   new_data <- deaths_filtered %>% filter(YEAR == year)
   
