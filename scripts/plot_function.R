d0092aec6fe2b2bd47f189f831da2c97b94c9bd5
EPAHANG <epahang@uw.edu>
2016-03-10 13:42:05 -0800

    Add comments to plot_function.R

--- scripts/plot_function.R
+++ scripts/plot_function.R
@@ -1,9 +1,9 @@
-# Creates a function that returns a line plot of given data
+# Creates functions that return a line plots of given data
 # setwd('~/Documents/INFO498F/final-project-INFO498F')
 
 # top_deaths <- read.csv('data/Top_Ten_US_Causes_Of_Death.csv')
 
-
+# Line plot for number of deaths per cause by year and location
 make_line_plot <- function(df, cause, location) {
   title_plot <- paste0("Number of Deaths by ", cause, " from 1999-2013 in ", location)
   df_filtered <- filter(df, CAUSE_NAME == cause, STATE == location)
@@ -12,6 +12,7 @@ make_line_plot <- function(df, cause, location) {
   return(p)
 }
 
+# Age adjusted death rates for the U.S. plot function
 make_age_adjusted_plot <- function(df, cause, location) {
   title_plot <- paste0("Age-Adjusted Death Rates for ", cause, ": ", location, " (1999-2013)")
   df_filtered <- filter(df, CAUSE_NAME == cause, STATE == location)
